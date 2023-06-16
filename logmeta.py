614db8d03008b06dd18d0bd0824bd8e938245535
sophia96ha <94056902+sophia96ha@users.noreply.github.com>
2023-06-16 21:03:25 +0700

    Create logmeta.py

---
+++ logmeta.py
@@ -0,0 +1,11 @@
+from lib2to3.pgen2 import driver
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium import webdriver
+import time
+import random
+import datetime
+#from termcolor import colored
